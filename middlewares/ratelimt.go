0c18b13761bba4966737d7d23efd8aa2229dbb45
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-08-16 23:13:51 +0800

    feat: update ratelimt middlewares

--- middlewares/ratelimt.go
+++ middlewares/ratelimt.go
@@ -1,8 +1,9 @@
 package middlewares
 
 import (
-	"go-sample/pkg/response"
 	"time"
+
+	"go-sample/pkg/response"
 
 	"github.com/gin-gonic/gin"
 	"github.com/juju/ratelimit"
@@ -11,13 +12,12 @@ import (
 func RateLimit(interval time.Duration, cap int64) func(c *gin.Context) {
 	bucket := ratelimit.NewBucket(interval, cap)
 	return func(c *gin.Context) {
-		// 如果取不到令牌就返回响应
 		if bucket.TakeAvailable(1) == 0 {
 			response.Error(c, response.TooManyRequests)
 			c.Abort()
 			return
 		}
-		// 取到令牌就放行
+
 		c.Next()
 	}
 }
