db567a0565f62359e3116c35869ff39162c38f50
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-09-05 23:35:17 +0800

    style: update aes utils

--- pkg/encrypt/aes.go
+++ pkg/encrypt/aes.go
@@ -4,8 +4,11 @@ import (
 	"crypto/aes"
 )
 
-func AesEncryptECB(src []byte, key []byte) (encrypted []byte) {
-	cipher, _ := aes.NewCipher(generateKey(key))
+func AesEncryptECB(src []byte, key []byte) ([]byte, error) {
+	cipher, err := aes.NewCipher(generateKey(key))
+	if err != nil {
+		return nil, err
+	}
 	length := (len(src) + aes.BlockSize) / aes.BlockSize
 	plain := make([]byte, length*aes.BlockSize)
 	copy(plain, src)
@@ -13,19 +16,21 @@ func AesEncryptECB(src []byte, key []byte) (encrypted []byte) {
 	for i := len(src); i < len(plain); i++ {
 		plain[i] = pad
 	}
-	encrypted = make([]byte, len(plain))
+	encrypted := make([]byte, len(plain))
 	// 分组分块加密
 	for bs, be := 0, cipher.BlockSize(); bs <= len(src); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
 		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
 	}
 
-	return encrypted
+	return encrypted, nil
 }
 
-func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte) {
-	cipher, _ := aes.NewCipher(generateKey(key))
-	decrypted = make([]byte, len(encrypted))
-	//
+func AesDecryptECB(encrypted []byte, key []byte) ([]byte, error) {
+	cipher, err := aes.NewCipher(generateKey(key))
+	if err != nil {
+		return nil, err
+	}
+	decrypted := make([]byte, len(encrypted))
 	for bs, be := 0, cipher.BlockSize(); bs < len(encrypted); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
 		cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
 	}
@@ -35,7 +40,7 @@ func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte) {
 		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
 	}
 
-	return decrypted[:trim]
+	return decrypted[:trim], nil
 }
 
 func generateKey(key []byte) (genKey []byte) {
