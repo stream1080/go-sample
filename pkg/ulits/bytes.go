aa02e59b1ddec310901b7d930689e258a5d762d1
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-09-18 23:15:49 +0800

    feat: add bytes equals check

---
+++ pkg/ulits/bytes.go
@@ -0,0 +1,18 @@
+package ulits
+
+// BytesEquals check whether the given bytes is equal
+func BytesEquals(a []byte, b []byte) bool {
+	if (a == nil && b != nil) || (a != nil && b == nil) {
+		return false
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	size := len(a)
+	for i := 0; i < size; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
