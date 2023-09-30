1722ad72ad0649a2f227486aedc6e01cbc9ae9dd
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-09-30 22:04:19 +0800

    feat: add node func

--- pkg/consistenthash/consistenthash.go
+++ pkg/consistenthash/consistenthash.go
@@ -1,6 +1,9 @@
 package consistenthash
 
-import "hash/crc32"
+import (
+	"hash/crc32"
+	"sort"
+)
 
 // HashFunc defines function to generate hash code
 type HashFunc func(data []byte) uint32
@@ -28,4 +31,19 @@ func NewNodeMap(fn HashFunc) *NodeMap {
 // IsEmpty returns if there is no node in NodeMap
 func (m *NodeMap) IsEmpty() bool {
 	return len(m.nodeHashs) == 0
+}
+
+// AddNode add the given nodes into consistent hash circle
+func (m *NodeMap) AddNode(keys ...string) {
+
+	for _, key := range keys {
+		if key == "" {
+			continue
+		}
+		hash := int(m.hashFunc([]byte(key)))
+		m.nodeHashs = append(m.nodeHashs, hash)
+		m.nodeHashMap[hash] = key
+	}
+
+	sort.Ints(m.nodeHashs)
 }
