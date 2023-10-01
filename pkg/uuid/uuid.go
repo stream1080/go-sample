89812c604a5303a5ca4a475cc28dbae2f6a7737a
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-10-01 23:07:34 +0800

    feat: add uuid v16 func

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -1,10 +1,18 @@
 package uuid
 
 import (
+	"strings"
+
 	uuid "github.com/satori/go.uuid"
 )
 
 // New 生成一个随机的唯一 ID
 func New() string {
 	return uuid.NewV4().String()
+}
+
+func UUID16() string {
+	uuidStr := New()
+	uuidStr = strings.ReplaceAll(uuidStr, "-", "")
+	return uuidStr[0:16]
 }
