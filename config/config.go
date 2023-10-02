6a2891fed173e28bdf202f82b8066eec181acf81
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-10-02 23:18:27 +0800

    refactor: use go-env parse config

--- config/config.go
+++ config/config.go
@@ -1,30 +1,30 @@
 package config
 
 type Config struct {
-	ServerConfig `yaml:"server"`
-	MySQLConfig  `yaml:"mysql"`
-	RedisConfig  `yaml:"redis"`
+	ServerConfig
+	MySQLConfig
+	RedisConfig
 }
 
 type ServerConfig struct {
-	Mode string `yaml:"mode"`
-	Port int    `yaml:"port"`
+	Mode string `env:"SERVER_MODE,default=debug"`
+	Port int    `env:"SERVER_PORT,default=8080"`
 }
 
 type MySQLConfig struct {
-	Host     string `yaml:"host"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
-	DB       string `yaml:"dbname"`
-	Port     int    `yaml:"port"`
+	Host     string `env:"MYSQL_HOST,default=127.0.0.1"`
+	User     string `env:"MYSQL_USER,default=sample"`
+	Password string `env:"MYSQL_PASSWORD,default=123456"`
+	DB       string `env:"MYSQL_DB,default=sample"`
+	Port     int    `env:"MYSQL_PORT,default=3306"`
 }
 
 type RedisConfig struct {
-	Host         string `yaml:"host"`
-	User         string `yaml:"user"`
-	Password     string `yaml:"password"`
-	Port         int    `yaml:"port"`
-	DB           int    `yaml:"db"`
-	PoolSize     int    `yaml:"pool_size"`
-	MinIdleConns int    `yaml:"min_idle_conns"`
+	Host         string `env:"REDIS_HOST,default=127.0.0.1"`
+	User         string `env:"REDIS_USER,default=sample"`
+	Password     string `env:"REDIS_PASSWORD,default=123456"`
+	Port         int    `env:"REDIS_PORT,default=6379"`
+	DB           int    `env:"REDIS_DB,default=0"`
+	PoolSize     int    `env:"REDIS_POOL_SIZE,default=100"`
+	MinIdleConns int    `env:"REDIS_MIN_IDLE_CONNS,default=20"`
 }
