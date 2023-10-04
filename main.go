6bad61d5038a559f4e7e2163993ab1e356261b79
stream1080 <77672403+stream1080@users.noreply.github.com>
2023-10-04 23:44:08 +0800

    style: remove main swagger docs

--- main.go
+++ main.go
@@ -13,20 +13,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server celler server.
-// @termsOfService https://github.com/stream108
-
-// @contact.name 一江溪水
-// @contact.url https://github.com/stream1080
-// @contact.email example.com
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host 127.0.0.1:8080
-// @BasePath /api/v1
 func main() {
 
 	global.InitConfig()
@@ -42,7 +28,7 @@ func main() {
 	go func() {
 		err := r.Run(fmt.Sprintf(":%d", global.Conf.ServerConfig.Port))
 		if err != nil && err != http.ErrServerClosed {
-			zap.S().Fatalf("listen: %s\n", err)
+			zap.S().Panicf("listen: %s\n", err)
 		}
 	}()
 
@@ -50,6 +36,5 @@ func main() {
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 
-	// TODO
 	zap.S().Info("Shutdown Server ...")
 }
