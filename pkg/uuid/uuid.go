626865430b03e51be45e2468ef953fce4e6fe237
stream1080 <77672403+stream1080@users.noreply.github.com>
2024-11-26 10:06:36 +0800

    feat: add id gen func

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -2,9 +2,34 @@ package uuid
 
 import (
 	"strings"
+	"sync"
+	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
+
+var (
+	letters    = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+	latestTime int64
+	count      int64
+	idMutex    sync.Mutex
+)
+
+func ID() uint64 {
+	idMutex.Lock()
+	defer idMutex.Unlock()
+	nowTime := time.Now().UnixNano() / 1e10
+	if latestTime == nowTime {
+		count++
+	} else {
+		latestTime = nowTime
+		count = 0
+	}
+	res := nowTime
+	res <<= 15
+	res += count
+	return uint64(res)
+}
 
 // New 生成一个随机的唯一 ID
 func New() string {
